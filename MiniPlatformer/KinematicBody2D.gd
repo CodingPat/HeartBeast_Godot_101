be5557c03c15280a46b752d8348c7cbf0ebc76b1
woody <woody@tallahassee.local>
2019-07-30 16:54:53 +0200

    miniplatformer spritesheet

--- MiniPlatformer/KinematicBody2D.gd
+++ MiniPlatformer/KinematicBody2D.gd
@@ -3,7 +3,13 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+const UP=Vector2(0,-1)
+const GRAVITY=20
+const SPEED=200
+const JUMP_HEIGHT=-600
+
 var motion=Vector2()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,13 +20,17 @@ func _ready():
 #	pass
 
 func _physics_process(delta):
-	motion.y+=1
+	motion.y+=GRAVITY
 	if Input.is_action_pressed("ui_right"):
-		motion.x=100
+		motion.x=SPEED
 	elif Input.is_action_pressed("ui_left"):
-		motion.x=-100
+		motion.x=-SPEED
 	else:
 		motion.x=0
 		
-	move_and_slide(motion)
+	if is_on_floor():
+		if Input.is_action_pressed("ui_up"):
+			motion.y+=JUMP_HEIGHT
+		
+	motion=move_and_slide(motion,UP)
 	
